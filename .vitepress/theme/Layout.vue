d1c610d581bd87b13d1eaf093759ab4c99388e98
LeadRDRK <leadrdrk@zohomail.com>
2026-01-17 21:23:32 +0700

    Thanks for everything

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,20 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import "./custom.css"
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+    <Layout>
+        <template #layout-top>
+            <div class="discontinue-notice">
+                <div class="inner">
+                    The original Hachimi project is no longer under active development.
+                    Please use a different mod.
+                    <a href="/goodbye">Learn more -></a>
+                </div>
+            </div>
+        </template>
+    </Layout>
+</template>
